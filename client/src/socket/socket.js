3791444d711065441ba3c6257de4d7f08ad1d2fa
LokiDarkness <lokijuliver@gmail.com>
2026-02-19 01:02:48 +0700

    Fix socket backend URL

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 
 const socket = io("https://game-tet-1.onrender.com", {
-  transports: ["websocket"]
+  transports: ["websocket"],
+  withCredentials: true
 });
 
 export default socket;
